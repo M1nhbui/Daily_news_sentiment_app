61edcf4f9a5992c8691df9b59e36af8b835bd08b
Minh Bui <buileminh342006@gmail.com>
2025-06-23 23:54:31 +0700

    Update tone_analyze.py

--- update_script/tone_analyze.py
+++ update_script/tone_analyze.py
@@ -1,7 +1,17 @@
-import pickle
 import os
+import pickle
+import requests
 
-MODEL_PATH = "train_model/prebuild_model.pkl"
+MODEL_PATH = "model/prebuild_model.pkl"
+MODEL_URL = "https://news-sentiment-290586778476.s3.amazonaws.com/prebuild_model.pkl"
+
+os.makedirs(os.path.dirname(MODEL_PATH), exist_ok=True)
+
+if not os.path.exists(MODEL_PATH):
+    print("🔽 Downloading model from S3...")
+    r = requests.get(MODEL_URL)
+    with open(MODEL_PATH, "wb") as f:
+        f.write(r.content)
 
 with open(MODEL_PATH, "rb") as f:
     model = pickle.load(f)
